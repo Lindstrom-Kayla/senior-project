604fa14fc0ce004eb82ec012c56aa8961df28464
Lindstrom-Kayla <lue11001@byui.edu>
2017-05-11 10:51:39 -0600

    Add files via upload

--- practice.php
+++ practice.php
@@ -19,35 +19,39 @@ $stmt->execute();
     <body>
         <div id="page">
             <ul>
-                <?php
-                while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+                <?php while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
                     $sub_sql = "SELECT * FROM submenu WHERE cat_id=:id";
                     $sub_stmt = $pdo->prepare($sub_sql);
                     $sub_stmt->bindParam(':id', $row->id,PDO::PARAM_INT);
-                    $sub_stmt->execute();
-                    ?>
+                    $sub_stmt->execute(); 
+                    $sub2_sql = "SELECT * FROM submenu2 WHERE cat_id=:id";
+                    $sub2_stmt = $pdo->prepare($sub2_sql);
+                    $sub2_stmt->bindParam(':id', $row->id,PDO::PARAM_INT);
+                    $sub2_stmt->execute(); ?>
+                
                 <li><a href=""><?php echo $row->name; ?></a>
-                <?php
-                if($sub_stmt->rowCount()){
-                ?>
-                <ul>
-                <?php
-                while($sub_row = $sub_stmt->fetch(PDO::FETCH_OBJ)) {
-                ?>
-                <li><a href="<?php echo $sub_row->href; ?>">
-                <?php echo $sub_row->name;?>
-                    </a></li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-                <?php
-                }
-                ?>
+                    <?php if($sub_stmt->rowCount()){ ?>
+                    
+                    <ul>
+                    <?php while($sub_row = $sub_stmt->fetch(PDO::FETCH_OBJ)) { ?>
+                        <li><a href="<?php echo $sub_row->href; ?>"> 
+                        <?php echo $sub_row->name;?></a>
+                        <?php if($sub2_stmt->rowCount()){ ?>
+                
+                            <ul>
+                            <?php while($sub2_row = $sub2_stmt->fetch(PDO::FETCH_OBJ)) { ?>
+                                <li><a href="<?php echo $sub2_row->href; ?>">
+                                <?php echo $sub2_row->sub_menu2;?></a></li>
+                            <?php } ?>
+                            </ul>
+                            
+                        <?php } ?>
+                        </li>
+                    <?php } ?>
+                    </ul>
+                    <?php } ?>
                 </li>
-                <?php
-                }
-                ?>
+                <?php } ?>
             </ul>
         </div>
     </body>
